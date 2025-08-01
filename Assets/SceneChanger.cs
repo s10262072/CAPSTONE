e18db52e7594bef035a33115f93524963aab5b72
s10262072 <s10262072@connect.np.edu.sg>
2025-08-01 11:44:05 +0800

    congrats you have finished your game

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -9,7 +9,7 @@ public class SceneChanger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
